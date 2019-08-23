39fdd300aba5bbbbdbe689984b8c06300e4085cb
Adam Brown <adam.brown.betty@gmail.com>
2019-08-23 13:52:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,14 +8,7 @@ def load_library(file_path)
     "get_emoticon" => {}, 
     "get_meaning" => {}
   }
- 
-emo_library.each do |meaning, emoticons|
-binding.pry
- hash["get_meaning"][emoticons] = meaning
 
-  hash["get_emoticon"][meaning] = emoticons
-
- end
 
 end 
 
