4067dc0e3f606c15e66228934e9c2a33da6d67d4
Adam Brown <adam.brown.betty@gmail.com>
2019-08-23 14:30:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,9 +19,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   new_hash = load_library(file_path)
-      if emo_library["get_emoticon"].include?(emoticon)
-        emo_library["get_emoticon"][emoticon]
-      end  
+     
 end
 
 def get_english_meaning
